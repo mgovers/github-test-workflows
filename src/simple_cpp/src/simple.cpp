6c5ae017e36f9102b8d56064b17b227116f154ba
Martijn Govers <Martijn.Govers@Alliander.com>
2023-10-25 09:55:18 +0200

    with feature test
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- src/simple_cpp/src/simple.cpp
+++ src/simple_cpp/src/simple.cpp
@@ -4,5 +4,12 @@
 
 int main() {
     constexpr auto r = std::array{0, 10, 15};
-    std::cout << "contains 10: " << static_cast<int>(std::ranges::find(r, 10) != std::cend(r)) << std::endl;
+
+    std::cout << "without ranges:\n";
+    std::cout << "\tcontains 10: " << static_cast<int>(std::find(std::cbegin(r), std::cend(r), 10) != std::cend(r)) << std::endl;
+
+#ifdef __cpp_lib_ranges
+    std::cout << "with ranges:\n";
+    std::cout << "\tcontains 10: " << static_cast<int>(std::ranges::find(r, 10) != std::cend(r)) << std::endl;
+#endif
 }
